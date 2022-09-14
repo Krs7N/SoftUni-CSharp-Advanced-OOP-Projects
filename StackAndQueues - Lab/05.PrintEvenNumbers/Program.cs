3f2809d16945eb8361b3d80732481f74f9433cc0
Kristian Mladenov <krstntank@gmail.com>
2022-09-15 00:01:22 +0300

    Added 3 new projects

---
+++ StackAndQueues - Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+
+            var queue = new Queue<int>();
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    queue.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
