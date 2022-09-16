3d1ccff56099635f94b575965a4705d30a85d2e1
Kristian Mladenov <krstntank@gmail.com>
2022-09-16 20:25:11 +0300

    Added first exercise from stack and queues exercises

---
+++ StackAndQueues - Exercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] numbersForStack = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var stack = new Stack<int>(numbersForStack);
+
+            int popCount = arr[1];
+            int numToLookFor = arr[2];
+
+            for (int i = 0; i < popCount; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(numToLookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
