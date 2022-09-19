a80f410f121fbcd3c2398dbe988464ca1cb0cacb
Kristian Mladenov <krstntank@gmail.com>
2022-09-19 14:52:30 +0300

    Added 2 more exercises

---
+++ StackAndQueues - Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string parenthesis = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            foreach (var ch in parenthesis)
+            {
+                if (stack.Any())
+                {
+                    char symbol = stack.Peek();
+
+                    if (symbol == '{' && ch == '}')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+
+                    if (symbol == '(' && ch == ')')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+
+                    if (symbol == '[' && ch == ']')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                }
+
+                stack.Push(ch);
+            }
+
+            Console.WriteLine(stack.Any() ? "NO" : "YES");
+        }
+    }
+}
---
+++ StackAndQueues - Exercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+
+            var text = new StringBuilder();
+            var memory = new Stack<string>();
+            memory.Push(string.Empty);
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                string[] commands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                switch (commands[0])
+                {
+                    case "1":
+                        text.Append(commands[1]);
+                        memory.Push(text.ToString());
+                        break;
+                    case "2":
+                        int count = int.Parse(commands[1]);
+                        text = text.Remove(text.Length - count, count);
+                        memory.Push(text.ToString());
+                        break;
+                    case "3":
+                        int index = int.Parse(commands[1]);
+                        string textString = text.ToString();
+                        Console.WriteLine(textString[index - 1]);
+                        break;
+                    case "4":
+                        memory.Pop();
+                        text = new StringBuilder(memory.Peek());
+                        break;
+                }
+            }
+        }
+    }
+}
