651bfd591495e727b2af1c670ea920af57fa480c
Kristian Mladenov <krstntank@gmail.com>
2022-10-19 13:27:03 +0300

    Added last 2 exercises from Sets And Dictionaries

---
+++ Sets and Dictionaries Advanced - Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carTokens = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            HashSet<string> cars = new HashSet<string>();
+
+            while (carTokens[0] != "END")
+            {
+                string command = carTokens[0];
+                string plateNumber = carTokens[1];
+
+                if (command == "IN")
+                {
+                    cars.Add(plateNumber);
+                }
+                else
+                {
+                    cars.Remove(plateNumber);
+                }
+
+                carTokens = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (!cars.Any())
+            {
+                Console.WriteLine("Parking Lot is Empty");
+
+                return;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, cars));
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/08.SoftUniParty/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var VIPs = new HashSet<string>();
+            var regulars = new HashSet<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "PARTY")
+            {
+                if (char.IsDigit(command[0]))
+                {
+                    VIPs.Add(command);
+                }
+                else
+                {
+                    regulars.Add(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (VIPs.Contains(command))
+                {
+                    VIPs.Remove(command);
+                }
+                else
+                {
+                    regulars.Remove(command);
+                }
+            }
+
+            Console.WriteLine(VIPs.Count + regulars.Count);
+
+            if (VIPs.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, VIPs));
+            }
+
+            if (regulars.Count > 0)
+            {
+
+                Console.WriteLine(string.Join(Environment.NewLine, regulars));
+            }
+        }
+    }
+}
