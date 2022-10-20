a7248d64fe435268734babbbcb0b1c1141d2be3b
Kristian Mladenov <krstntank@gmail.com>
2022-10-20 22:27:45 +0300

    Added more exercises from Functional Programming Exercises

---
+++ Functional Programming - Exercise/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.AppliedArithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Func<List<int>, List<int>> add = numbers =>
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numbers[i]++;
+                }
+
+                return numbers;
+            };
+
+            Func<List<int>, List<int>> subtract = numbers =>
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numbers[i]--;
+                }
+
+                return numbers;
+            };
+
+            Func<List<int>, List<int>> multiply = numbers =>
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numbers[i] *= 2;
+                }
+
+                return numbers;
+            };
+
+            Action<List<int>> print = names => Console.WriteLine(string.Join(' ', names));
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        add(numbers);
+                        break;
+                    case "multiply":
+                        multiply(numbers);
+                        break;
+                    case "subtract":
+                        subtract(numbers);
+                        break;
+                    case "print":
+                        print(numbers);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Functional Programming - Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Reverse().ToList();
+            int numberToDivideWith = int.Parse(Console.ReadLine());
+
+            Func<List<int>, List<int>> divider = numbers =>
+            {
+                List<int> result = new List<int>();
+
+                foreach (var number in numbers)
+                {
+                    if (number % numberToDivideWith != 0)
+                    {
+                        result.Add(number);
+                    }
+                }
+
+                return result;
+            };
+
+            Console.WriteLine(string.Join(' ', divider(numbers)));
+        }
+    }
+}
