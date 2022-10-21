de7f54e79ad7678820ab218c6195d6987ea14346
Kristian Mladenov <krstntank@gmail.com>
2022-10-21 16:54:39 +0300

    Added first exercises from Defining Classes

---
+++ Defining Classes - Exercise/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03.OldestFamilyMember
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int countOfInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfInputs; i++)
+            {
+                string[] personData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Person oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
---
+++ Defining Classes - Exercise/DefiningClasses/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+
+            Person secondPerson = new Person(20);
+
+            Console.WriteLine(secondPerson.Name);
+            Console.WriteLine(secondPerson.Age);
+
+            Person thirdPerson = new Person("George", 15);
+
+            Console.WriteLine(thirdPerson.Name);
+            Console.WriteLine(thirdPerson.Age);
+        }
+    }
+}
